213c2256677adf49024a15d7b0a51b3bf279d1be
Florian Magalhaes <m.f.pro@outlook.com>
2021-03-09 17:57:48 +0100

    Logique Vue Product faite - Ajout de Splide

---
+++ src/components/ProductDetailed.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+    <div class="card mb-3">
+      <span>
+      <slot name="image"></slot>
+      </span>
+      <div class="card-body">
+        <h5 class="card-title"><slot name="title"></slot></h5>
+        <p class="card-text">
+          <slot name="description"></slot>
+        </p>
+        <p class="card-text">
+          <slot name="price"></slot>
+        </p>
+        <router-link :to="{ name: 'Product' }" class="btn btn-primary"
+          >Ajouter au panier</router-link
+        >
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "ProductDetailed",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,17 +1,49 @@
 <template>
-<article>
-  </article>
+  <div class="container">
+    <ProductDetailed>
+      <template v-slot:image>
+        <splide :options="options">
+          <splide-slide>
+            <img :src="product.image1" class="" alt="..." />
+          </splide-slide>
+          <splide-slide>
+            <img :src="product.image2" class="" alt="..." />
+          </splide-slide>
+          <splide-slide>
+            <img :src="product.image3" class="" alt="..." />
+          </splide-slide>
+        </splide>
+      </template>
+      <template v-slot:title>{{ product.title }}</template>
+      <template v-slot:description>{{ product.long_description }}</template>
+      <template v-slot:price>{{ product.price }} $</template>
+    </ProductDetailed>
+  </div>
 </template>
 
 <script>
+import ProductDetailed from "@/components/ProductDetailed.vue";
+
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 
 export default {
   name: "Product",
   components: {
+    ProductDetailed,
+    Splide,
+    SplideSlide
   },
   data: () => {
     return {
-      products: [],
+      product: {},
+      options: {
+        rewind: true,
+        gap: "1rem",
+        arrows: true,
+        pagination: true,
+        perPage: 1,
+      },
     };
   },
   mounted() {
@@ -19,7 +51,10 @@ export default {
       .get(`https://florian-magalhaes.fr/MOCK_DATA.json`)
       .then((data) => {
         const products = data.data;
-        this.products = products.filter(product => product.id == this.$route.params.id);
+        const oneProduct = products.filter(
+          (product) => product.id == this.$route.params.id
+        );
+        this.product = oneProduct[0];
       })
       .catch((e) => {
         console.log(e);
@@ -27,3 +62,11 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+li {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
