26acbabf04f228a8f66e0470b548dd9590d7343d
Florian Magalhaes <m.f.pro@outlook.com>
2021-03-09 13:22:23 +0100

    Ajout du router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Products/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Products from '@/components/Products.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Products
+  }
+}
+</script>
