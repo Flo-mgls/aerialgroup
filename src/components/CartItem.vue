438c588174e561e07989ea6142ab3280f538ca12
Florian Magalhaes <m.f.pro@outlook.com>
2021-03-09 20:13:22 +0100

    Ajout de la logique de la vue Cart

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,5 +1,13 @@
 <template>
-<div></div>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span
+      ><slot name="title"></slot
+      > <span class="text-muted"> (<slot name="price"></slot> $ / pc)</span></span
+    >
+    <span class="badge bg-primary rounded-pill"
+      ><slot name="quantity"></slot
+    ></span>
+  </li>
 </template>
 
 <script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-      <CartItem/>
+    <ul class="list-group">
+      <CartItem v-for="product in cartDisplayed" :key="product.title">
+        <template v-slot:title>{{ product.title }}</template>
+        <template v-slot:price>{{ product.price }}</template>
+        <template v-slot:quantity>{{ product.quantity }}</template>
+      </CartItem>
+    </ul>
+    <p>Total: {{ totalPrice }} $</p>
   </div>
 </template>
 
@@ -14,13 +21,50 @@ export default {
   },
   data: () => {
     return {
+      cart: {},
+      cartDisplayed: [],
     };
   },
+  methods: {
+    getCart() {
+      const currentCart = JSON.parse(localStorage.getItem("cart"));
+      for (let i = 0; i < currentCart.length; i++) {
+        let product = currentCart[i];
+        this.cart[product] = this.cart[product] ? this.cart[product] + 1 : 1;
+      }
+    },
+  },
+  computed: {
+      totalPrice() {
+          let totalPrice = 0;
+          this.cartDisplayed.forEach(function(product) {
+              totalPrice += product.price * product.quantity;
+          })
+
+          return totalPrice
+      }
+  },
   mounted() {
+    this.getCart();
     this.$axios
       .get(`https://florian-magalhaes.fr/MOCK_DATA.json`)
       .then((data) => {
-        this.products = data.data;
+        const products = data.data;
+        for (const productID in this.cart) {
+          let product = {};
+          const productsFilter = products.filter(
+            (product) => product.id == productID
+          );
+          const productTitle = productsFilter[0].title;
+          const productPrice = productsFilter[0].price;
+          const productQuantity = this.cart[productID];
+
+          product.title = productTitle;
+          product.price = productPrice;
+          product.quantity = productQuantity;
+          this.cartDisplayed.push(product);
+          console.log(this.cartDisplayed)
+        }
       })
       .catch((e) => {
         console.log(e);
